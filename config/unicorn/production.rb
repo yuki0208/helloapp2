980e1e2378d1b6ce25dbc2b3b7eda7c796ed61a6
yuki <yukkixsu8@yahoo.co.jp>
2017-07-27 16:09:14 +0900

    unicorn sock

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,8 +2,8 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 2)
 timeout 15
 preload_app true  # 更新時ダウンタイム無し
 
-listen "/tmp/unicorn.sock"
-pid "/tmp/unicorn.pid"
+listen File.expand_path('../../../tmp/unicorn.sock', __FILE__)
+pid File.expand_path('../../../tmp/unicorn.pid', __FILE__)
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
