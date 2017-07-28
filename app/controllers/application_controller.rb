8fe5d70f1b1ac595aa7a772d5f94f927b0d2ebf8
yuki <yukkixsu8@yahoo.co.jp>
2017-07-28 12:45:44 +0900

    hello,worldに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def goodbye
-    render html: "hola, mundo!"
+    render html: "hello, world!"
   end
 end
